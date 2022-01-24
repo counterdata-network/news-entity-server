bad9e7c5f7feeefbae0048a7f770d31bd4d4660b
Rahul B <rahulbot@gmail.com>
2022-01-24 10:41:50 -0500

    fix english model loading

--- helpers/entities.py
+++ helpers/entities.py
@@ -8,7 +8,7 @@ from helpers.exceptions import UnknownLanguageException
 
 # lookup table that maps from language code to default spaCy NER model
 language_nlp_lookup = {
-    ENGLISH: spacy.load("en_core_web_sm" if MODEL_MODE == MODEL_MODE_SMALL else "en_core_web_lg"),
+    ENGLISH: spacy.load("en_core_web_sm" if MODEL_MODE == MODEL_MODE_SMALL else "en_core_web_trf"),
     SPANISH: spacy.load("es_core_news_sm" if MODEL_MODE == MODEL_MODE_SMALL else "es_core_news_lg"),
     PORTUGUESE: spacy.load("pt_core_news_sm" if MODEL_MODE == MODEL_MODE_SMALL else "pt_core_news_lg"),
     FRENCH: spacy.load("fr_core_news_sm" if MODEL_MODE == MODEL_MODE_SMALL else "fr_core_news_lg"),
